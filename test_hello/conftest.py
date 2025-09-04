07040da77a7455c74c14357c34148edbb4b05a91
prehisle <prehisle@gmail.com>
2025-09-04 10:41:16 +0800

    开干前

--- test_hello/conftest.py
+++ test_hello/conftest.py
@@ -1,13 +1,9 @@
 import pytest
 
 from olymat.utils.ctx_helper import make_default_ctx
-from olymat.utils.ssh_helper import init_ssh_fixtures
 
 from .config import get_g_conf
 
-init_ssh_fixtures(globals(), get_g_conf().env.servers, groups=[
-    ["all", ["main", "pkgs"]],
-])
 
 @pytest.fixture(scope="session")
 def cfg():
